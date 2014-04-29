b720a69217dcf8c0a6ff24706459513ec1e0d7f1
Marin <marin@stayke.net>
2014-04-29 15:05:17 -0700

    fix single file torrents that use "name" key. #2

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 
 func lstorrent(path string) {
     fmt.Printf("%s\n", path)
-    defer fmt.Printf("\n\n")
+    defer fmt.Printf("\n")
     f, err := os.OpenFile(path, os.O_RDONLY, 0600)
 
 
@@ -37,11 +37,29 @@ func lstorrent(path string) {
         return
     }
 
-    files := datb["info"].(map[string]interface{})["files"].([]interface{})
+    tor_info, ok := datb["info"].(map[string]interface{})
+    if !ok {
 
-    for _, v := range files {
-        file := v.(map[string]interface{})
-        path := file["path"].([]interface{})[0].(string)
-        fmt.Printf("%s\n", path)
     }
+
+    // first look for files key
+    files, ok := tor_info["files"].([]interface{})
+    if ok {
+        for _, v := range files {
+            file := v.(map[string]interface{})
+            path := file["path"].([]interface{})[0].(string)
+            fmt.Printf("%s\n", path)
+        }
+
+        return // finished printing multifile torrent
+    }
+
+    // second look for name key
+    file, ok := tor_info["name"].(string)
+    if ok {
+        fmt.Printf("%s\n", file)
+        return
+    }
+
+    fmt.Printf("couldnt find files in torrent: %s", path)
 }
